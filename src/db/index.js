dd75183c5ee314d64e10a0757f2738fd6f7818ab
Matheus Almeida <mat.almeida@live.com>
2019-01-30 03:30:19 -0300

    fckng env variables

--- src/db/index.js
+++ src/db/index.js
@@ -12,13 +12,30 @@ const sequelize = new Sequelize({
   port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  database: process.env.POSTGRES_DB,
   pool: {
     max: 10,
     min: 0,
     acquire: 30000,
     idle: 10000
+  },
+  retry: {
+    match: [
+      /SequelizeConnectionError/,
+      /SequelizeConnectionRefusedError/,
+      /SequelizeHostNotFoundError/,
+      /SequelizeHostNotReachableError/,
+      /SequelizeInvalidConnectionError/,
+      /SequelizeConnectionTimedOutError/
+    ],
+    name: "query",
+    backoffBase: 100,
+    backoffExponent: 1.1,
+    timeout: 60000,
+    max: Infinity
   }
 });
+
+sequelize.authenticate().then();
 
 export { sequelize };
