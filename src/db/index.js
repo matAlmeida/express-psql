7570aab60a6134ace46637d2ebd05f4dc8f1194b
Matheus Almeida <mat.almeida@live.com>
2019-01-30 02:17:23 -0300

    changing env variables again

--- src/db/index.js
+++ src/db/index.js
@@ -3,16 +3,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   define: {
     timestamps: true
   },
-  dialect: "postgres",
-  database: process.env.POSTGRES_DATABASE,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
   pool: {
     max: 10,
     min: 0,
